2997c43be8679b8d72aff6e4d4685786e6dc4170
Santiago Saavedra <s.saavedra@fdi.ucm.es>
2016-04-22 19:27:14 +0200

    Add copyright headers

--- runtime/builtins.lisp
+++ runtime/builtins.lisp
@@ -1,3 +1,24 @@
+;;; CAVIART-VCGEN - A verification condition generator for the CAVI-ART project
+;;; developed originally at GPD UCM.
+;;; Copyright (C) 2016 Santiago Saavedra López, Grupo de Programación Declarativa -
+;;; Universidad Complutense de Madrid
+;;;
+;;; This file is part of CAVIART-VCGEN.
+;;;
+;;; CAVIART-VCGEN is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU Affero General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; CAVIART-VCGEN is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU Affero General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Affero General Public License
+;;; along with CAVIART-VCGEN.  If not, see <http://www.gnu.org/licenses/>.
+
+
 (cl:in-package :cl-user)
 (defpackage :ir.rt.builtins
   (:use :ir.rt.core :common-lisp)
